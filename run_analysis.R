0ca3c32f6f4b9231537fcb78452a80a8fe74e6c1
pusayliu <101928990+pusayliu@users.noreply.github.com>
2022-04-24 20:23:35 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,3 @@
-setwd('D:/RStudio/UCIHARDataset')
 X_train <- read.table("./train/X_train.txt")
 y_train <- read.table("./train/y_train.txt")
 X_test <- read.table("./test/X_test.txt")
@@ -10,19 +9,13 @@ features <- read.table("./features.txt")
 activity_labels <- read.table("./activity_labels.txt")
 colnames(df)[1:2] <- c('activity', 'subject')
 colnames(df)[3:dim(df)[2]] <- features[,2]
-# body_acc_x_train <- read.table("./train/Inertial Signals/body_acc_x_train.txt")
-# body_acc_y_train <- read.table("./train/Inertial Signals/body_acc_y_train.txt")
-# body_acc_z_train <- read.table("./train/Inertial Signals/body_acc_z_train.txt")
-# body_acc_x_test <- read.table("./test/Inertial Signals/body_acc_x_test.txt")
-# body_acc_y_test <- read.table("./test/Inertial Signals/body_acc_y_test.txt")
-# body_acc_z_test <- read.table("./test/Inertial Signals/body_acc_z_test.txt")
+
 meanstd <- colnames(df)[grep("[Mm]ean|std", colnames(df))]
 df <- df[,c(colnames(df)[1:2],meanstd)]
 
 for (i in 1:length(df$activity)){
   df$activity[i] = activity_labels[activity_labels$V1 == df$activity[i],]$V2
 }
-
 
 library(stringr)
 for (i in 1:length(colnames(df))){
@@ -37,7 +30,3 @@ df_group <- aggregate(x = df, by = list(df$activity, df$subject), FUN = "mean")
 colnames(df_group)[1:2] = colnames(df_group)[3:4]
 df_group <- df_group[-c(3:4)]
 write.csv(df_group,".\\HAR.csv", row.names = FALSE)
-
-#unique(na.omit(str_match(colnames(df), "[tf](.*?)-")[,1]))
-# unique(na.omit(str_match(colnames(df), "^t(.*?)-")[,1]))
-# unique(na.omit(str_match(colnames(df), "^f(.*?)-")[,1]))
